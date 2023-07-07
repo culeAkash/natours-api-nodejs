49daf7cd23d9e780d0f201f15d74019882388c08
Akash Jaiswal <akashjais929@gmail.com>
2023-07-07 07:30:26 +0530

    Implementing better error handling in the project
    Refactored our error handling code in the controlllers, set up diff errors for both and client and dev.
    
    Please see TODO in errorController.js

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,4 +1,44 @@
 // Global error handling middleware
+
+
+// TODO : NOW, WHENEVER THERE IS AN ERROR GIVEN BY MONGOOSE, LIKE VALIDATION OR INVALID ID THEY ARE NOT THROWN BY US. SO, THE ISOPERATIONAL IS NULL THERE BUT WE ALSO WANT TO SHOW DETAILS ABOUT THOSE ERRORS IN PRODUCTION. WE WILL TAKE CARE OF THAT IN NEXT STEPS
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack
+  })
+}
+
+const sendErrorProd = (err, res) => {
+  // ! Operational, trusted errors, send message to client
+
+  // ? We have defined the isOperational parameter in the AppError class and whenever there is a error defined by us then isOperational = true is set, so we can trust that error, to be sent to client as well
+
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.statusCode,
+      message: err.message
+    })
+  }
+
+  // ! Programming or other unknown error, don't leak error details to client in prod
+
+  else {
+    // TODO : Log the error to the console
+    console.log('Error : ', err);
+
+    // ? Send generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong'
+    })
+  }
+}
+
+
 module.exports = (err, req, res, next) => {
 
   console.log(err.stack);
@@ -6,8 +46,23 @@ module.exports = (err, req, res, next) => {
   const statusCode = err?.statusCode || 500;
   const status = err?.status || 'error';
 
-  res.status(statusCode).json({
-    status: status,
-    message: err.message ? err.message : "Something went wrong"
-  })
+
+  // * we want to send errors based on environment
+  const currEnv = process.env.NODE_ENV;
+
+  if (currEnv === 'development') {
+    sendErrorDev(err, res);
+  }
+  else if (currEnv === 'production') {
+    sendErrorProd(err, res);
+  }
 }
+
+
+/*
+ * NOW WE WANT TO SEND DIFFERENT ERROR MESSAGES FOR PRODUCTION AND DEVELOPMENT ENVIRONMENTS
+
+ ! WE WANT TO DO THAT AS WE DON'T WANT THE CLIENT IN DEVELOPMENT TO SEE MUCH DATA ABOUT THE ERROR, BUT IN DEV ENV WE WAN'T TO SEE MORE DETAILS ABOUT THE ERROR
+ 
+ ! WE WILL BE DOING THAT USING THE PROCESS.ENV.NODE_ENV VARIABLE THAT WE HAVE SET IN THE CONFIG.ENV FILE
+ */
--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,6 +4,8 @@
 const fs = require('fs');
 const Tour = require('../models/tourModel')
 const ApiFeatures = require('../utils/ApiFeatures')
+const catchAsync = require('../utils/CatchAsync');
+const AppError = require('../utils/AppError');
 
 
 //For testing only
@@ -42,73 +44,80 @@ exports.aliasTopTours = (req, res, next) => {
 
 
 // method for getting all routes
-exports.getAllTours = async (req, res, next) => {
+exports.getAllTours = catchAsync(async (req, res, next) => {
 
-  try {
+  // try {
 
-    // EXECUTE QUERY
-    const features = new ApiFeatures(Tour.find(), req.query)
-      .filter()
-      .sort()
-      .limitFields()
-      .paginate();
+  // EXECUTE QUERY
+  const features = new ApiFeatures(Tour.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
 
-    // const query = Tour.find()
-    // .where('duration').equals(5)
-    // .where('difficulty').equals('easy');
+  // const query = Tour.find()
+  // .where('duration').equals(5)
+  // .where('difficulty').equals('easy');
 
 
-    const tours = await features.mongoQuery;
-    // query.sort().select().skip().limit()
-    // All these methods return query which can ba again chained with other query methods and then awaited to get our final results
+  const tours = await features.mongoQuery;
+  // query.sort().select().skip().limit()
+  // All these methods return query which can ba again chained with other query methods and then awaited to get our final results
 
-    //SEND RESPONSE
-    //Now documents are fetched from DB
-    res.status(200).json({
-      status: 'success',
-      results: tours.length,
-      data: {
-        tours
-      }
-    })
-  } catch (error) {
-    res.status(500).json({
-      status: 'fail',
-      message: error
-    })
-  }
+  //SEND RESPONSE
+  //Now documents are fetched from DB
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours
+    }
+  })
+  // } catch (error) {
+  //   res.status(500).json({
+  //     status: 'fail',
+  //     message: error
+  //   })
+  // }
 
-}
+})
 
 // Get tour by id handler
-exports.getTourById = async (req, res, next) => {
+exports.getTourById = catchAsync(async (req, res, next) => {
   //variables are present in req.params wrapped in an object
 
   console.log(req.params);
 
   const reqId = req.params.id;
 
-  try {
-    const tour =
-      // await Tour.findById(reqId);
-      await Tour.findOne({ _id: req.params.id })
+  // try {cat
+  const tour =
+    // await Tour.findById(reqId);
+    await Tour.findOne({ _id: req.params.id })
 
-    res.status(200).json({
-      status: 'success',
-      data: {
-        tour
-      }
-    })
-  } catch (error) {
-    res.status(500).json({
-      status: 'fail',
-      message: error
-    })
+  if (!tour) {
+    return next(new AppError(`No tour found with id: ${req.params.id}`, 404));
   }
-}
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour
+    }
+  })
+  // } catch (error) {
+  //   res.status(500).json({
+  //     status: 'fail',
+  //     message: error
+  //   })
+  // }
+})
+
+
+
 
 // Create new tour handler
-exports.createNewTour = async (req, res, next) => {
+exports.createNewTour = catchAsync(async (req, res, next) => {
   // We have to add a middleware for this to work
   console.log(req.body);
 
@@ -117,175 +126,194 @@ exports.createNewTour = async (req, res, next) => {
 
   // Creating tour using mongoose and real database
   //Directly create document without creating model object
-  try {
-    //If any validation fails or any other error while adding to database
-    const newTour = await Tour.create(req.body);
-    res.status(201).json({
-      status: 'success',
-      data: {
-        tour: newTour
-      }
-    })
-    //error will be catched here
-  } catch (error) {
-    res.status(400).json({
-      status: 'fail',
-      message: error
-    })
-  }
+  // try {
+  //If any validation fails or any other error while adding to database
+  const newTour = await Tour.create(req.body);
+  res.status(201).json({
+    status: 'success',
+    data: {
+      tour: newTour
+    }
+  })
+  //error will be catched here
+  // } catch (error) {
+  //   res.status(400).json({
+  //     status: 'fail',
+  //     message: error
+  //   })
+  // }
 
-}
+})
 
 //Update Tour handler
 
-exports.updateTour = async (req, res, next) => {
+exports.updateTour = catchAsync(async (req, res, next) => {
 
-  try {
-    const reqId = req.params.id;
+  // try {
+  const reqId = req.params.id;
 
-    //find doc by id and update the doc
-    const updatedTour = await Tour.findByIdAndUpdate(reqId, req.body, {
-      //To return the new updated doc as response
-      new: true,
-      // validation takes place again before updation in database
-      runValidators: true
-    });
+  //find doc by id and update the doc
+  const updatedTour = await Tour.findByIdAndUpdate(reqId, req.body, {
+    //To return the new updated doc as response
+    new: true,
+    // validation takes place again before updation in database
+    runValidators: true
+  });
 
-    res.status(200).json({
-      status: 'success',
-      data: {
-        updatedTour
-      }
-    });
-  } catch (error) {
-    res.status(400).json({
-      status: 'fail',
-      message: error
-    })
+
+  if (!updatedTour) {
+    return next(new AppError(`No Tour is found with id : ${req.params.id}`, 404));
   }
 
+  res.status(200).json({
+    status: 'success',
+    data: {
+      updatedTour
+    }
+  });
+  // } catch (error) {
+  //   res.status(400).json({
+  //     status: 'fail',
+  //     message: error
+  //   })
+  // }
 
 
 
-}
+
+})
 
 
 //Delete tour handler
-exports.deleteTour = async (req, res, next) => {
+exports.deleteTour = catchAsync(async (req, res, next) => {
 
-  try {
-    const reqId = req.params.id;
-    await Tour.findByIdAndDelete(reqId);
-    res.status(204).json({
-      status: 'success',
-    })
-  } catch (error) {
-    res.status(400).json({
-      status: 'fail',
-      message: error
-    })
+  // try {
+  const reqId = req.params.id;
+  const tour = await Tour.findByIdAndDelete(reqId);
+
+  if (!tour) {
+    return next(new AppError(`No Tour found with id: ${reqId}`, 404));
   }
-}
 
 
-exports.getTourStats = async (req, res, next) => {
+  res.status(204).json({
+    status: 'success',
+  })
+  // } catch (error) {
+  //   res.status(400).json({
+  //     status: 'fail',
+  //     message: error
+  //   })
+  // }
+})
 
-  try {
-    const stats = await Tour.aggregate([
-      //stages : doc will pass through all these stages
-      {
-        $match: {
-          ratingsAverage: { $gt: 4.5 }
-        }
-      },
-      {
-        $group: {
-          // _id: null,
-          // _id: '$ratingsAverage',
-          // _id: '$difficulty',
-          _id: { $toUpper: '$difficulty' },
-          numTours: { $sum: 1 },
-          numRatings: { $sum: '$ratingsQuantity' },
-          avgRating: { $avg: '$ratingsAverage' },
-          avgPrice: { $avg: '$price' },
-          minPrice: { $min: '$price' },
-          maxPrice: { $max: '$price' }
-        }
-      }, {
-        $sort: { avgPrice: 1 }
-      },
-      {
-        $match: { _id: { $ne: "EASY" } }
+
+exports.getTourStats = catchAsync(async (req, res, next) => {
+
+  // try {
+  const stats = await Tour.aggregate([
+    //stages : doc will pass through all these stages
+    {
+      $match: {
+        ratingsAverage: { $gt: 4.5 }
       }
-    ])
-
-
-    res.status(200).json({
-      status: 'success',
-      data: {
-        stats
+    },
+    {
+      $group: {
+        // _id: null,
+        // _id: '$ratingsAverage',
+        // _id: '$difficulty',
+        _id: { $toUpper: '$difficulty' },
+        numTours: { $sum: 1 },
+        numRatings: { $sum: '$ratingsQuantity' },
+        avgRating: { $avg: '$ratingsAverage' },
+        avgPrice: { $avg: '$price' },
+        minPrice: { $min: '$price' },
+        maxPrice: { $max: '$price' }
       }
-    });
-  } catch (error) {
-    res.status(400).json({
-      status: 'fail',
-      message: error
-    })
-  }
-}
+    }, {
+      $sort: { avgPrice: 1 }
+    },
+    {
+      $match: { _id: { $ne: "EASY" } }
+    }
+  ])
 
 
-exports.getMonthlyPlan = async (req, res, next) => {
-  try {
-    const year = req.params.year * 1;//2021
-    const plan = await Tour.aggregate([
-      {
-        $unwind: '$startDates'
-      },
-      {
-        $match: {
-          startDates: {
-            $gte: new Date(`${year}-01-01`),
-            $lte: new Date(`${year}-12-31`) //end of
-          }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      stats
+    }
+  });
+  // } catch (error) {
+  //   res.status(400).json({
+  //     status: 'fail',
+  //     message: error
+  //   })
+  // }
+})
+
+
+exports.getMonthlyPlan = catchAsync(async (req, res, next) => {
+  // try {
+  const year = req.params.year * 1;//2021
+  const plan = await Tour.aggregate([
+    {
+      $unwind: '$startDates'
+    },
+    {
+      $match: {
+        startDates: {
+          $gte: new Date(`${year}-01-01`),
+          $lte: new Date(`${year}-12-31`) //end of
         }
-      },
-      {
-        $group: {
-          _id: { $month: '$startDates' },
-          numTours: { $sum: 1 },
-          tours: { $push: '$name' }
-        }
-      }, {
-        $addFields: { month: '$_id' }
-      }, {
-        $project: {
-          _id: 0
-        }
-      }, {
-        $sort: { numTours: -1 }// sort by number of tour in descending order
-      }, {
-        $limit: 12// return only top n records if specified
       }
-    ])
-
-
-    res.status(200).json({
-      status: 'success',
-      size: plan.length,
-      data: {
-        plan
+    },
+    {
+      $group: {
+        _id: { $month: '$startDates' },
+        numTours: { $sum: 1 },
+        tours: { $push: '$name' }
       }
-    });
-  } catch (error) {
-    res.status(400).json({
-      status: 'fail',
-      message: error
-    })
-  }
-}
+    }, {
+      $addFields: { month: '$_id' }
+    }, {
+      $project: {
+        _id: 0
+      }
+    }, {
+      $sort: { numTours: -1 }// sort by number of tour in descending order
+    }, {
+      $limit: 12// return only top n records if specified
+    }
+  ])
 
 
+  res.status(200).json({
+    status: 'success',
+    size: plan.length,
+    data: {
+      plan
+    }
+  });
+  // } catch (error) {
+  //   res.status(400).json({
+  //     status: 'fail',
+  //     message: error
+  //   })
+  // }
+})
+
+/**
+ * * LOGIC BEHIND OUR GLOBAL EXCEPTION HANDLING DONE BY CATCHASYNC MODULE THAT WE HAVE CREATED IN THE PROJECT
+ *
+ * ? STEP 1 => WHENEVER A HANDLER IS CALLED FROM THE ROUTES MODULE, THE HANDLER WILL CALL THE CATCHASYNC FUNCTION THAT WE HAVE CREATED IN THE CATCHASYNC MODULE.
+ *
+ * ? STEP 2 => NOW THE CATCHASYNC METHOD WILL RETURN A FUNCTION WHICH WILL CALL THE ASYNC FUNCTION PASSED FROM THE CONTROLLER TO THE CATCHASYNC METHOD.
+ *
+ * ? STEP 3 => NOW SINCE THE FUNCTION PASSED FROM HANDLER IS AN ASYNC FUNCTION, IT'S PROMISE IT CATCHED IN CASE OF AN ERROR AND GLOBAL ERROR HANDLING IS CALLED BY THE NEXT(ERROR).
+ */
 
 
 
@@ -305,3 +333,7 @@ exports.getMonthlyPlan = async (req, res, next) => {
 //   }
 //   next();
 // }
+
+
+
+
